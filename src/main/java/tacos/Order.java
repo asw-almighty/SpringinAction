bbd90e77e9ea77e5ba62ffcbb68cfeda8f4ca76c
안승우 <rj9101@jeit.co.kr>
2020-06-12 17:49:33 +0900

    ch03

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -5,10 +5,15 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.Date;
 
 @Data
 public class Order {
+    private Long id;
+    private Date placedAt;
+
     @NotBlank(message="required")
     private String name;
 
